858acce733a7c2068e904e6883466565da7e70be
Bolphunga <aymendj@yahoo.com>
2023-12-06 18:29:40 +0100

    uploaded images

--- constants/mydb.js
+++ constants/mydb.js
@@ -4,7 +4,7 @@ export const LocalProjects = [
     {
         title: "Madaura: Student Share Website",
         description: "Welcome to Madaura, a fully-responsive web application platform for students to lend, borrow, sell, and buy new and used items of different categories from each other.",
-        images: ["https://previews.dropbox.com/p/thumb/ACFg2Xl3WEPUAgL-oJ_mBXKHCTOT1Blcx1fuhSv9SG1p-YtjuCSyy7aeSnb9lBqyYU8oiw8K1K1nY_n-lq_-vNsK-dPHmIzCBSfd7T0EMTcdwywMDGbSfNwcPjyhLmmaKkd7jPPDxnPy7LvvS9qhGrCaQmo2ybd__k2P8pRlQWavdol5LPwIVO3VYr9vdCknyKTGY6D3s8v0yXm6Ue_miVooslR6WCyO8GbT58rMsDMUW-cn8zSYRzTjAkJ4B7l4YOLomV5SMBcPx59ebCgFv6flKVry6DZO7ax-x_iq9-lHXH3gxn2LrwLY0yNYtpFxk5HaWVz6mnCUR2l_0jQYiFiW/p.jpeg", "https://previews.dropbox.com/p/thumb/ACG-hacY8p19Bw15vx_eU_Hoiqn8A4wUzkf2IRjgJWoBkRApeKIYgKJ78ZY2eGmuA9eYsz9o38YE1ZzO7rqo-JcY2YkyJijobYei04LAkuYxEDXlou1TQaeOpbDHPUIxy8BUMhnDJX2GMqTnDjS8xM4dkhHJtVxwFJ3jNxxw1fT23T4Nn1xeZLyFd2e3ox4XxGVEhvwMTcWZWFGxKhrQBURSJxcUJRKimO1b-rOacvaqZ3t8fZ0XPrOHtiAlf82I0koNRwT_1EBQEZo7DYOsv6HmEmA5yjeQx1qCqhtTCSbIX7byR-uMBcNYmMbyfkLHTs2fHSLgg2ayj1MZZrzw840R/p.jpeg", "https://previews.dropbox.com/p/thumb/ACFspuyQdy1F7H0Q85KqrFfC_2gi4YnR88TH0vSDFkcjY_NWPUlWx6zhx1zzeAvOayLovg_o3YLQmlJ8qWKjUyk1GgmwBDcCfvKAqB7tx3z4Dc6st09AsCRpJQhIetO3cUULA1lo9cAB2cSFi-9rhq3rm2VwnvrRf0fTQT8D0O5qcAxEU1wNQzj5Mr2NL8ixHgs4ODdvTQlmQ7PZcvkKhPtB3Ia7FNBhpuaDYYwfr1KSUf3USZD56Ob2JSCPA3FfSYxEoIX94tfMFPDzDoQLWg6yn4XoCIS2inhC1QrlzZDfig7IjqY7VAAHoWxOJd04lz32P8uZlndZIcScQvYAoRGq/p.jpeg"],
+        images: ["/Madaura1.jpg", "/Madaura2.jpg", "/Madaura3.jpg"],
         liveUrl: "student-store-team-2.vercel.app",
         repoUrl: "https://github.com/202306-NEA-DZ-FEW/student-store-team-2",
         stack: ["Next.js", "TailwindCSS", "Supabase", "Firebase", "React Leaflet", "Cloudinary", "next-intl", "Jest"],
@@ -12,7 +12,7 @@ export const LocalProjects = [
     {
       title: "So9ify: E-commerce Website",
       description: "So9ify is an e-commerce website project that demonstrates the development of a functional online shopping platform. It provides users with the ability to browse, filter, products. The project focuses on building the frontend of the website using Next.js and Tailwind CSS.",
-      images: ["https://shorturl.at/gBK47", "https://shorturl.at/rsMP0", "https://shorturl.at/jHLN0"],
+      images: ["/So9ify1.jpg", "So9ify2.jpg", "So9ify3.jpg"],
       liveUrl: "",
       repoUrl: "https://github.com/Bolphunga/e-commerce-project-team-8",
       stack: ["Next.js", "Firebase", "TailwindCSS", "API"],
@@ -20,7 +20,7 @@ export const LocalProjects = [
     {
       title: "Cinepholie: Movie Database",
       description: "Cinepholie leverages the TMDB (The Movie Database) API to present comprehensive information about movies and actors. Users can explore a rich database, accessing details about movies, including synopses, cast information, and other relevant data. The project showcases the integration of Next.js to build a dynamic and responsive web application that seamlessly retrieves and displays content from the TMDB API, offering an immersive experience for movie enthusiasts.",
-      images: ["https://previews.dropbox.com/p/thumb/ACGV6U6PYQcnKVU2L2A6iXLGastRKtnheuiW8WYCIER0-fVabZbre4EqPNcvwha9U2bHVb6bVoIVjOMQbokrhhI9Lu1-W2XG1ZJYeRZvffzFRwk0Z1YrXWWsJE30Ht3fW_aSHJDpMmhY8ekHtTgL9bFCNElHoEmyaola-DgkpoGjsPBuQgpgszHr3G-SDiKhWnWuHU8G1CjD5zSSnGUtoDcJai78RLmmdaORoh0eTlnl1Divi0FPGlOvyk48odpCHsOhjdOawIUzKjLzQ-VSvFG1ezD1gvG7L8GvSj3gHTGGQy4ynbev_8NoHzXF10p2N-6Nnh2wXVt41TF4ZVdPs93M/p.jpeg", "https://previews.dropbox.com/p/thumb/ACHv5EEPJ7Ehl7pSpkOwQj7qPVU77LH8TraNPo5tBnhIZmK8oyed5W5QeGzznMJM8S57OMqnOnp8NkTO4mtLxaWsMIdkeczulzcEmjWbiU0WbjYJOJoI311JAF_FidffHKAMUbNRUSdBeoFxL8eK4HqFduQBNiBBk558wNpYjUOjG1IKdjtynuf9BgG6zqeD98YliwlAoN8EgqgV1vx-PaOdPMCGzgg0IK8etZesDWda3GTV4cX1EAReyT-LYMxY2LihQngHrGvmP_kxE7ms7ESsjWnzewEAe57r5GFcIOuJO8wMCDKBYPEHBkHn9JLPzE43YBdkBaipE9FUrHMS4gmt/p.jpeg", "https://previews.dropbox.com/p/thumb/ACFeiIg5mjHGDjeYvSVkMEtU5P8KB1jp1bjQ0yi5KJYjxkVfkpPx46hJzXf1RJ0MO5fWyPkkcE9LNhzuKAPHXjoRCxBlq2PQlMS3BtkD5TDFtOF-NVhqgxRn1jBL73eavu_lS1G-2E00Lo0WHyAHzpbs8NjjWVpXBHEdRDRoCDD-GvovbQQXcaYOcUYFFODUhVYOeO1uE4EqWb3bynZZXBsrcgzLuOkHWtXeJz6Xsqgl5gqF8lFbWeFWTTDUzVFv6ECuEJ4Epwj0qPZxxByKe0zBKih_4_Aqab3sN7jG8YTj3ZeHvxPwi-5jUcyW4Wm1motk2cIaGhilSJuGUWzIZSwA/p.jpeg"],
+      images: ["/Cine1.jpg", "/Cine2.jpg", "/Cine3.jpg"],
       liveUrl: "movie-project-cinephiles.vercel.app",
       repoUrl: "https://github.com/202306-NEA-DZ-FEW/movie-project-cinephiles",
       stack: ["Next.js", "REST API", "TailwindCSS", "NextUI", "MaterialUI"],
