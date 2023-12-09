a2d58bef080ccd2d4ba4dad64ac7e7e8adfef968
Bolphunga <aymendj@yahoo.com>
2023-12-10 00:21:40 +0100

    link fix

--- constants/mydb.js
+++ constants/mydb.js
@@ -5,7 +5,7 @@ export const LocalProjects = [
         title: "Madaura: Student Share Website",
         description: "Welcome to Madaura, a fully-responsive web application platform for students to lend, borrow, sell, and buy new and used items of different categories from each other.",
         images: ["/Madaura1.jpg", "/Madaura2.jpg", "/Madaura3.jpg"],
-        liveUrl: "student-store-team-2.vercel.app",
+        liveUrl: "https://student-store-team-2.vercel.app",
         repoUrl: "https://github.com/202306-NEA-DZ-FEW/student-store-team-2",
         stack: ["Next.js", "TailwindCSS", "Supabase", "Firebase", "React Leaflet", "Cloudinary", "next-intl", "Jest"],
       },
@@ -21,7 +21,7 @@ export const LocalProjects = [
       title: "Cinepholie: Movie Database",
       description: "Cinepholie leverages the TMDB (The Movie Database) API to present comprehensive information about movies and actors. Users can explore a rich database, accessing details about movies, including synopses, cast information, and other relevant data. The project showcases the integration of Next.js to build a dynamic and responsive web application that seamlessly retrieves and displays content from the TMDB API, offering an immersive experience for movie enthusiasts.",
       images: ["/Cine1.jpg", "/Cine2.jpg", "/Cine3.jpg"],
-      liveUrl: "movie-project-cinephiles.vercel.app",
+      liveUrl: "https://movie-project-cinephiles.vercel.app",
       repoUrl: "https://github.com/202306-NEA-DZ-FEW/movie-project-cinephiles",
       stack: ["Next.js", "REST API", "TailwindCSS", "NextUI", "MaterialUI"],
     } 
